d97f3aff7e765c6662c3e2fdcdabea78b40a4fbe
BrentBauman86 <brentbauman86@gmail.com>
2019-01-29 05:13:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
+let store = { drivers: [], passengers: [], trips: [] };
 
-let store = { drivers: [], passengers: [], trips: []}
-
-let driverId = 0
-let passengerId = 0
-let tripId = 0 
-
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
 
 class Driver {
-    constructor(name){
-    this.name = name
-    this.id = ++driverId  
-    
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+
     store.drivers.push(this);
   }
+  
   trips() {
-      return store.trips.filter(function(trip){
-          return trip.driverId === this.id;
-      }.bind(this) 
-    );
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
   }
 
   passengers() {
-      return store.passengers.filter(function(passenger){
-        return passenger.driverId === this.id;
-        }.bind(this)
-      });
-   }
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
 }
 
 class Passenger {
-    constructor(name) {
-    this.name = name 
-    this.id = ++passengerId
-    
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+
     store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
   }
 }
 
 class Trip {
-    constructor(driverId, passengerId){
-        this.pid = ++passengerId;  
-        this.did = ++driverId;
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
 
-        store.trips.push(this);
-    } 
+    store.trips.push(this);
+  }
+
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 }
